f41893e8171734931522c25003ced14d605f82b2
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2020-03-24 21:09:17 -0400

    Versio 1.0

--- scripts/map.js
+++ scripts/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([43.45228, -80.5060], 13);
+var mymap = L.map('mapid').setView([0, 0], 2);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -8,12 +8,21 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiY3JubWFwIiwiYSI6ImNrODBrbHIwejAwazkzam50M3M1ZzgzZHIifQ.2rjlC0umbYG2UdW4wil4XQ'
 }).addTo(mymap);
 
-//add a cirle to the map
-var circle = L.circle([43.45228, -80.5060], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 5000
-}).addTo(mymap);
+//Fetch coronavirus data
+var dataUrl = "https://corona.lmao.ninja/countries?sort=%7Bparameter%7D";
 
-circle.bindPopup("Active case: 9<br> Recovered case: 4<br> Death: 1");
+fetch(dataUrl).then((response)=>{
+    return response.json();
+}).then(function(data){
+    for(var i = 0; i < data.length; i++){
+        var circle = L.circle([data[i].countryInfo.lat, data[i].countryInfo.long], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 15000+(data[i].cases*5)
+        }).addTo(mymap);
+
+        circle.bindPopup("Country: " + data[i].country + "<br> Total case: " + data[i].cases + "<br> Recovered case: " + data[i].recovered + 
+        "<br> Death: " + data[i].deaths);
+    }
+});
